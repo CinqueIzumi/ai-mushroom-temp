87407c6c1db9949bdc91201ca21bc6963eea76b0
CinqueIzumi <nofeednolife@gmail.com>
2021-11-04 18:51:34 +0100

    [Updated] Injection container

--- lib/injection_container.dart
+++ lib/injection_container.dart
@@ -1,10 +1,12 @@
+import 'dart:io';
+
 import 'package:ai_mushroom/features/mushroom_pred/data/datasources/remote_data_source.dart';
 import 'package:ai_mushroom/features/mushroom_pred/data/repositories/mushroom_repository_impl.dart';
 import 'package:ai_mushroom/features/mushroom_pred/domain/repositories/mushroom_repository.dart';
 import 'package:ai_mushroom/features/mushroom_pred/domain/usecases/get_prediction.dart';
 import 'package:ai_mushroom/features/mushroom_pred/presentation/bloc/mushroom_bloc.dart';
 import 'package:get_it/get_it.dart';
-import 'package:http/http.dart' as http;
+import 'package:http/io_client.dart';
 
 final sl = GetIt.instance;
 
@@ -19,8 +21,12 @@ Future<void> init() async {
   sl.registerLazySingleton<MushroomRepository>(() => MushroomRepositoryImpl(dataSource: sl()));
 
   // Data sources
-  sl.registerLazySingleton<RemoteDataSource>(() => RemoteDataSourceImpl(client: sl()));
+  sl.registerLazySingleton<RemoteDataSource>(() => RemoteDataSourceImpl(http: sl()));
 
   // External
-  sl.registerLazySingleton(() => http.Client());
+  // sl.registerLazySingleton(() => http.Client());
+  final ioc = HttpClient();
+  ioc.badCertificateCallback = (X509Certificate cert, String host, int port) => true;
+  final http = IOClient(ioc);
+  sl.registerLazySingleton(() => http);
 }
