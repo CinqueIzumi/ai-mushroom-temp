a16d31e5e41ed1ad32561b9834691ca2388d8cb7
CinqueIzumi <nofeednolife@gmail.com>
2021-11-04 11:43:58 +0100

    [Added] Injection Container

---
+++ lib/injection_container.dart
@@ -0,0 +1,25 @@
+import 'package:ai_mushroom/features/mushroom_pred/data/datasources/remote_data_source.dart';
+import 'package:ai_mushroom/features/mushroom_pred/data/repositories/mushroom_repository_impl.dart';
+import 'package:ai_mushroom/features/mushroom_pred/domain/usecases/get_prediction.dart';
+import 'package:ai_mushroom/features/mushroom_pred/presentation/bloc/mushroom_bloc.dart';
+import 'package:get_it/get_it.dart';
+import 'package:http/http.dart' as http;
+
+final sl = GetIt.instance;
+
+Future<void> init() async {
+  // Bloc
+  sl.registerFactory(() => MushroomBloc(getPrediction: sl()));
+
+  // Use cases
+  sl.registerLazySingleton(() => GetPrediction(sl()));
+
+  // Repository
+  sl.registerLazySingleton(() => MushroomRepositoryImpl(dataSource: sl()));
+
+  // Data sources
+  sl.registerLazySingleton<RemoteDataSource>(() => RemoteDataSourceImpl(client: sl()));
+
+  // External
+  sl.registerLazySingleton(() => http.Client());
+}
